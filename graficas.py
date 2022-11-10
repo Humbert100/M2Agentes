214a547a56b25f2629a22df2f13a6b0188c4556f
Humberto Alejandro Rosas Téllez <93540685+Humbert100@users.noreply.github.com>
2022-11-09 23:56:18 -0600

    Add files

--- graficas.py
+++ graficas.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 ########################################################
 
-#SEGUNDA GRAFICA
+#PRIMERA GRAFICA
 all_clean = []
 # This runs the model 100 times, each model executing 10 steps.
 for j in range(200):
@@ -24,7 +24,7 @@ plt.show()
 
 ########################################################
 
-#TERCERA GRÁFICA
+#SEGUNDA GRÁFICA
 agent_counts = np.zeros((model.grid.width, model.grid.height))
 for cell in model.grid.coord_iter():
     cell_content, x, y = cell
@@ -35,7 +35,7 @@ plt.colorbar()
 plt.show()
 
 ########################################################
-
+#TERCERA GRÁFICA
 
 # params = {"width": 10, "height": 10, "N": range(10, 250, 10), "M": range(10,300,10)}
 
@@ -57,13 +57,3 @@ plt.show()
 # gini_values = results_filtered.Gini.values
 # plt.scatter(N_values, gini_values)
 # plt.show()
-
-
-
-# one_episode_wealth = results_df[(results_df.N == 10) & (results_df.iteration == 2)]
-# # Then, print the columns of interest of the filtered data frame
-# print(
-#     one_episode_wealth.to_string(
-#         index=False, columns=["Step", "AgentID", "Limpieza"], max_rows=25
-#     )
-# )
